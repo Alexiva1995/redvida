e2734e862da1d246fb69c43bff90f76b3746275f
luisana <alexisjoseva95@gmail.com>
2021-03-31 09:23:55 -0400

    diseño dashboard completo

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,8 +1,22 @@
 @extends('layouts.dashboard')
 
 @push('scripts')
-	<script>
+	<script type="text/javascript">
 		$(window).on("load", function () {
+			$('.zero-configuration').DataTable({
+				dom: 'tp',
+				ordering: false,
+				pageLength : 6,
+				language: {
+					"paginate": {
+						"first": "Primero",
+						"last": "Ultimo",
+						"next": "<i class='fas fa-angle-right'></i>",
+						"previous": "<i class='fas fa-angle-left'></i>"
+		            }	
+		        },
+			});
+
 			//Get the context of the Chart canvas element we want to select
   			var lineChartctx = $("#line-chart");
 
@@ -40,18 +54,20 @@
 		  	};
 
 		  	// Chart Data
+		  	var comisionesTotales = <?= json_encode($arrayComisionesTotales) ?>;
+		  	var comisionesPagadas = <?= json_encode($arrayComisionesPagadas) ?>;
 		  	var linechartData = {
-		    	labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+		    	labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
 		    	datasets: [
 		    	{
-			      	label: "Africa",
-			      	data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+			      	label: "Comisiones Totales",
+			      	data: comisionesTotales,
 			      	borderColor:'#255EBA',
 			      	fill: false
 		    	},
 		    	{
-			      	data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
-			      	label: "Asia",
+		    		label: "Comisiones Pagadas",
+			      	data: comisionesPagadas,
 			      	borderColor: '#34C900',
 			      	fill: false
 		    	}]
@@ -66,9 +82,49 @@
 
   			// Create the chart
   			var lineChart = new Chart(lineChartctx, lineChartconfig);
+
+  			// Doughnut Chart
+  			// ---------------------------------------------
+  			//Get the context of the Chart canvas element we want to select
+  			var doughnutChartctx = $("#simple-doughnut-chart");
+
+  			// Chart Options
+  			var doughnutchartOptions = {
+    			responsive: true,
+    			maintainAspectRatio: false,
+    			responsiveAnimationDuration: 500,
+    			legend: {
+		    		display: false,
+		    	},
+    			/*title: {
+      				display: true,
+      				text: 'Predicted world population (millions) in 2050'
+    			}*/
+  			};
+
+			// Chart Data
+			var doughnutchartData = {
+			    labels: ["Activos", "Inactivos"],
+			    datasets: [{
+			      	label: "My First dataset",
+			      	data: <?= json_encode($cantReferidos) ?>,
+			      	backgroundColor: ['#34C900', '#8B8B8B'],
+			    }]
+			};
+
+		  	var doughnutChartconfig = {
+		    	type: 'doughnut',
+		    	// Chart Options
+		    	options: doughnutchartOptions,
+		    	data: doughnutchartData
+		  	};
+
+  			// Create the chart
+  			var doughnutSimpleChart = new Chart(doughnutChartctx, doughnutChartconfig);
 		});
 	</script>	
 @endpush
+
 @section('content')
 	<div class="contai2">
 		<div class="row">
@@ -76,6 +132,7 @@
 			@if (Auth::user()->rol_id == 0)
 				@include('dashboard.componenteIndex.admin_square')
 			@else
+				{{-- Primera Columna--}}
 				<div class="col-lg-6 col-md-12 col-sm-12">
 					<div class="card bg-analytics text-white">
 						<div class="card-content">
@@ -132,7 +189,7 @@
 					                    	Saldo Actual
 					                    </div>
 					                    <div style="color: #34C900; font-size: 25px; font-weight: 700;">
-					                    	{{ Auth::user()->wallet_amount }}$
+					                    	{{ number_format(Auth::user()->wallet_amount, 2, '.', ',') }}$
 					                    </div>
 									</div>
 									<div class="col-6">
@@ -144,6 +201,7 @@
 					</div>
 				</div> 
 
+				{{-- Segunda Columna--}}
 				<div class="col-lg-12 col-md-12 col-sm-12">
 					<div class="card">
 					 	<div class="card-header">
@@ -164,119 +222,118 @@
                         </div>
                     </div>
 				</div>
+
+				{{-- Tercera Columna--}}
+				<div class="col-lg-6 col-md-12 col-sm-12">
+					<div class="card" style="min-height: 500px;">
+						<div class="card-header">
+						 	<div style="color: #3C3232; font-weight: 600; font-size: 20px;">
+						 		Usuarios
+	                        </div>
+	                    </div>
+                        <div class="card-content">
+                        	<div class="card-body">
+                        		<div class="height-300">
+                        			<canvas id="simple-doughnut-chart"></canvas>
+                        		</div>
+                        		<div class="row mt-3" style="color: #3C3232; font-weight: 500;">
+                        			<div class="col-6">
+                        				<i class="fa fa-circle font-small-3 mr-50" style="color: #34C900;"></i> Usuarios Activos
+                        			</div>
+                        			<div class="col-6 text-right">
+                        				{{ $cantReferidos[0] }}
+                        			</div>
+                        			<div class="col-6 mt-1">
+					 					<i class="fa fa-circle font-small-3 mr-50" style="color: #8B8B8B;"></i> Usuarios Inactivos
+					 				</div>
+					 				<div class="col-6 mt-1 text-right">
+					 					{{ $cantReferidos[1] }}
+					 				</div>
+                        		</div>
+                        	</div>
+                        </div>
+                    </div>
+				</div>
+				<div class="col-lg-6 col-md-12 col-sm-12" >
+					<div class="card" style="min-height: 500px;">
+						<div class="card-header">
+						 	<div style="color: #3C3232; font-weight: 600; font-size: 20px;">
+						 		Últimos registros directos
+	                        </div>
+	                    </div>
+                        <div class="card-content">
+                        	<div class="card-body">
+                        		<div class="table-responsive ">
+                                    <table class="table zero-configuration">
+                                    	<thead>
+                                    		<tr>
+                                    			<th class="text-center">ID</th>
+                                    			<th class="text-center">NOMBRE</th>
+                                    			<th class="text-center">ESTADO</th>
+                                    		</tr>
+                                    	</thead>
+                                    	<tbody>
+                                    		@foreach ($ultRegistrosDirectos as $registroDirecto)
+	                                    		<tr>
+	                                    			<td class="text-center">{{ $registroDirecto->ID }}</td>
+	                                    			<td class="text-center">{{ (!empty($registroDirecto->display_name)) ? $registroDirecto->display_name : $registroDirecto->user_email }}</td>
+	                                    			<td class="text-center">
+	                                    				@if ($registroDirecto->status == 1)
+						                        			<i class="fa fa-circle font-small-3 text-success mr-50"></i> Activo
+	                                    				@else
+						                        			<i class="fa fa-circle font-small-3 text-danger mr-50"></i> Inactivo
+	                                    				@endif
+	                                    			</td>
+	                                            </tr>
+	                                        @endforeach
+	                                    </tbody>
+	                                </table>
+                                </div>
+                        	</div>
+                        </div>
+                    </div>
+				</div>
+
+				{{-- Cuarta Columna --}}
+				<div class="col-lg-12 col-md-12 col-sm-12" >
+					<div class="card">
+						<div class="card-header">
+						 	<div style="color: #3C3232; font-weight: 600; font-size: 20px;">
+						 		Últimos pedidos
+	                        </div>
+	                    </div>
+                        <div class="card-content">
+                        	<div class="card-body">
+                        		<div class="table-responsive ">
+                                    <table class="table zero-configuration">
+                                    	<thead>
+                                    		<tr>
+                                    			<th class="text-center">ID</th>
+                                    			<th class="text-center">NOMBRE</th>
+                                    			<th class="text-center">FECHA</th>
+                                    			<th class="text-center">CANTIDAD</th>
+                                    			<th class="text-center">ACCIÓN</th>
+                                    		</tr>
+                                    	</thead>
+                                    	<tbody>
+                                    		<tr>
+                                    			<td class="text-center">1</td>
+	                                    		<td class="text-center">Lorem Ipsum</td>
+	                                    		<td class="text-center">01/01/2021</td>
+	                                    		<td class="text-center">6</td>
+	                                    		<td class="text-center">
+	                                    			<a href="" style="color: #3C3232;"><i class="fa fa-eye mr-50"></i></a>
+	                                    			<a href="" style="color: #3C3232;"><i class="fa fa-trash mr-50"></i></a>
+	                                    		</td>
+	                                    	</tr>
+	                                    </tbody>
+	                                </table>
+                                </div>
+                        	</div>
+                        </div>
+                    </div>
+				</div>
 			@endif
-			{{-- secundo cuadro --}}
-			@if ($data['paquete'] == 0)
-				@include('dashboard.componenteIndex.modalGold')
-			@endif
-			{{-- tecer cuadro --}}
-			{{-- @include('dashboard.componenteIndex.third_square') --}}
 		</div>
 	</div>
 @endsection
-
-@push('custom_js')
-<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-<script src="{{asset('assets/scripts/graficas.js')}}"></script>
-
-<script type="text/javascript">
-	function copyToClipboard(element) {
-		var aux = document.createElement("input");
-		aux.setAttribute("value", document.getElementById(element).innerHTML.replace('&amp;', '&').trim());
-		document.body.appendChild(aux);
-		aux.select();
-		document.execCommand("copy");
-		document.body.removeChild(aux);
-		Swal.fire({
-			title: '¡Link Copiado!',
-			text: "Su link de referido esta listo para pegar",
-      		type: "success",
-			confirmButtonClass: 'btn btn-primary',
-			buttonsStyling: false,
-			}).then((value) => {
-				if (value) {
-					window.location.reload()
-				}
-			})
-	}
-
-	/**
-	* Permite modificar el lado binario donde se van a ir registrando los usuarios
-	*/
-	function updateSideBinary(value) {
-		let url = "{{route('change.side')}}"
-		let valor = value
-		let data = {
-			ladoregistrar: valor,
-			_token: "{{ csrf_token() }}",
-		}
-		let lado = (valor == 'D') ? 'Derecha' : 'Izquierda'
-		$.post(url, data, function(response){
-			if (response = 1) {
-				Swal.fire({
-				title: 'Lado Binario Actualizado',
-				text: "Su nuevo lado de registro binario es por la "+ lado,
-				type: "success",
-				confirmButtonClass: 'btn btn-primary',
-				buttonsStyling: false,
-				}).then((value) => {
-					if (value) {
-						copyToClipboard('copy')
-					}
-				})
-			}else{
-				Swal.fire({
-				title: 'Error',
-				text: "No se pudo actualizar el lado a registrar intente de nuevo",
-				type: "danger",
-				confirmButtonClass: 'btn btn-primary',
-				buttonsStyling: false,
-			}).then((value) => {
-				if (value) {
-					window.location.reload()
-				}
-			})
-			}
-		})
-	}
-
-	function fbs_click(publi) {
-		u=publi.img;
-		// t=document.title;
-   		t=publi.title
-		let urlCom = "{{route('publicidad.compartido')}}"
-		let url = 'http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t)
-   		window.open(url, 'sharer', 'toolbar=0,status=0,width=626,height=436')
-		data = {
-			id: publi.id,
-			social: 'facebook',
-			_token: '{{ csrf_token() }}'
-        }
-		$.post(urlCom, data, function(){
-			alert('compartido')
-			window.location.reload()
-		})
-		return false;
-	}
-</script>
-
-@if ($data['paquete'] == 0)
-<script>
-	$(document).ready(function(){
-		$('#myModalGold').modal('show')
-	})
-</script>
-@endif
-@endpush
-
-@push('vendor_css')
-<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/charts/apexcharts.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/extensions/sweetalert2.min.css')}}">
-<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
-@endpush
-
-@push('page_vendor_js')
-<script src="{{asset('app-assets/vendors/js/charts/apexcharts.min.js')}}"></script>
-<script src="{{asset('app-assets/vendors/js/extensions/sweetalert2.all.min.js')}}"></script>
-@endpush
--- resources/views/layouts/include/scripts.blade.php
+++ resources/views/layouts/include/scripts.blade.php
@@ -9,6 +9,14 @@
 <script src="{{asset('app-assets/vendors/js/charts/chart.min.js') }}"></script>
 <script src="{{asset('app-assets/vendors/js/extensions/tether.min.js')}}"></script>
 <script src="{{asset('app-assets/vendors/js/extensions/shepherd.min.js')}}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/pdfmake.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/vfs_fonts.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/datatables.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/datatables.buttons.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/buttons.print.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/buttons.bootstrap.min.js') }}"></script>
+<script src="{{ asset('app-assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js') }}"></script>
 @stack('page_vendor_js')
 <!-- END: Page Vendor JS-->
 
